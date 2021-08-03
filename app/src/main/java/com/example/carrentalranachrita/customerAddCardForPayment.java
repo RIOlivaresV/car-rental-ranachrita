98195d8d2ac5737a843e9a29e1a5fcfbdbe28f4a
tnikdel <51806955+tnikdel@users.noreply.github.com>
2021-08-03 14:54:49 -0700

    3 August - Payment page added version. clicking on pay will show a message

--- app/src/main/java/com/example/carrentalranachrita/customerAddCardForPayment.java
+++ app/src/main/java/com/example/carrentalranachrita/customerAddCardForPayment.java
@@ -3,12 +3,29 @@ package com.example.carrentalranachrita;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class customerAddCardForPayment extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_add_card_for_payment);
-    }
+
+        btn = findViewById(R.id.btnPayThePayment);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),"Your payment was successful!" , Toast.LENGTH_LONG).show();
+
+            }
+
+        });
+}
 }
